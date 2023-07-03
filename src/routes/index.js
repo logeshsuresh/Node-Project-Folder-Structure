8f62f7a58f46f69fcba3e701825bf0113731d292
Logesh Suresh <logeshsrinivasans@gmail.com>
2023-07-03 19:42:52 +0530

    Added sequelize and CLI related file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const v1Routes = require('./v1');
 
+
 router.use('/v1', v1Routes);
+
 
 module.exports = router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { infoController, InfoController } = require('../../controllers');
+const { InfoController } = require('../../controllers');
 
 router.get('/info', InfoController.info);
 
